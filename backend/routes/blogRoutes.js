ea0643739210abe8595dc1e57ebd4c9036adb83d
jahidul96 <jahidul59895@gmail.com>
2022-11-10 19:22:16 +0600

    tag search added

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -131,7 +131,9 @@ router.put("/blog/comment/:id", async (req, res, next) => {
 router.get("/searchbytag/:tag", async (req, res, next) => {
   console.log(req.params.tag);
   try {
-    const blogs = await Blog.find({ tags: { $in: [req.params.tag] } });
+    const blogs = await Blog.find({ tags: { $in: [req.params.tag] } }).populate(
+      "postedBy"
+    );
 
     res.status(200).json({
       status: "succes",
